232c7b8be3b057d28ca1c6746e3c348ba0b1fd25
mjkahnamu2 <mj.kahnamuee@gmail.com>
2022-03-10 22:03:20 +0330

    haminjuri

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
+from django.views.generic import TemplateView
 from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('search/', SearchResultsView.as_view(), name='post_search'),
+    path('تماس با من/', TemplateView.as_view(template_name='blog/contact.html'), name='contact_me'),
     re_path('(?P<slug>[-\w]+)/', post_detail, name='post_detail'),
 ]
