7aad0e7aad6cecbf2ca3d69cc616d14deccfa7f4
mjkahnamu2 <mj.kahnamuee@gmail.com>
2022-03-15 14:27:01 +0330

    another commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hm_k5%%6p3tij^i#@y%ndjnx+@vdjrj%=dki0m*z+xg9v-w_+1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
